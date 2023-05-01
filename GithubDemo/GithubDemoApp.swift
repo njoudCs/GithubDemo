1ffe1c97d96a1698d1da49b29b79fd411162c36b
njoudCs <njoud.cs.pnu@gmail.com>
2023-05-01 15:25:18 +0300

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Njoud saud Al-Najem on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
